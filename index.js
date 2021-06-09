cf34a7c46ff079256ef88174eb0ac07ae22da6c9
Ayush-jft <Ayush.sahu@jellyfishtechnologies.com>
2021-06-09 23:25:55 +0530

    final commit for selenium script

--- index.js
+++ index.js
@@ -1,26 +1,65 @@
 require("chromedriver");
 const selenium = require("selenium-webdriver");
-const { username, password } = require("./creds.json");
-
+const { username, password, userNameAst } = require("./creds.json");
 
 const browser = new selenium.Builder();
-const tab = browser.forBrowser('chrome').build();
+const tab = browser.forBrowser("chrome").build();
 
-let openTab = tab.get("http://www.way2automation.com/angularjs-protractor/registeration/#/login");
-openTab.then(() => {
-    let findTimeOut =
-        tab.manage().setTimeouts({
-            implicit: 10000
-        });
+let openTab = tab.get(
+  "http://www.way2automation.com/angularjs-protractor/registeration/#/login"
+);
+openTab
+  .then(() => {
+    let findTimeOut = tab.manage().setTimeouts({
+      implicit: 10000,
+    });
     return findTimeOut;
-        }).then(() => {
-            let userNamePromise =
-                tab.findElement(selenium.By.id("username"));
-            return userNamePromise;
-        }).then((userNameInput)=>{
-            let inputUserName = userNameInput.sendKeys(username);
-            return inputUserName;
-        }).then(()=>{
-            console.log("user name input sucessfull.")
-        });
-    
+  })
+  .then(() => {
+    let userNamePromise = tab.findElement(selenium.By.id("username"));
+    return userNamePromise;
+  })
+  .then((userNameInput) => {
+    let inputUserName = userNameInput.sendKeys(username);
+    return inputUserName;
+  })
+  .then(() => {
+    console.log("user name input sucessfully.");
+
+    let passwordPromise = tab.findElement(selenium.By.id("password"));
+    return passwordPromise;
+  })
+  .then((passwordInput) => {
+    let inputPassword = passwordInput.sendKeys(password);
+    return inputPassword;
+  })
+  .then(() => {
+    console.log("password input successfully");
+
+    let userNameAsteriskPromise = tab.findElement(
+      selenium.By.id("formly_1_input_username_0")
+    );
+    return userNameAsteriskPromise;
+  })
+  .then((userNameAsteriskInput) => {
+    let inputUsernameAsterisk = userNameAsteriskInput.sendKeys(userNameAst);
+    return inputUsernameAsterisk;
+  })
+  .then(() => {
+    console.log("username asterisk input successfully");
+
+    let SignInButtonPromise = tab.findElement(
+      selenium.By.className("btn btn-danger")
+    );
+    return SignInButtonPromise;
+  })
+  .then((signInButton) => {
+    let promiseClickSignIn = signInButton.click();
+    return promiseClickSignIn;
+  })
+  .then(() => {
+    console.log("Successfully Signed In!");
+  })
+  .catch((err) => {
+    console.log("Error ", err, " occurred!");
+  });
